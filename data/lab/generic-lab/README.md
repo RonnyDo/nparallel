e8e7c803686ed10d4ecdcccaa30b457fb2b2aa55
RonnyDo <Ronny Dobra 6539575+RonnyDo@users.noreply.github.com>
2025-04-25 01:43:59 +0200

    use single router instead limiting all targets

--- data/lab/generic-lab/README.md
+++ data/lab/generic-lab/README.md
@@ -1,16 +1,15 @@
 # Nparallal Lab 🧪
 
-The nparallel lab simulates a network with different subnets and hosts. It's meant to compare performance if nparallel with other network tools, like nmap.
+The nparallel lab simulates a network with different subnets and hosts. It's meant to compare performance of nparallel with other network tools, like nmap.
 
-The lab not only simulates the structure of a network but also real-life parameters like bandwidth and response delay. 
-
+The lab not only simulates the structure of a network but also real-life parameters like bandwidth and response delay. This is done by a router which connects the scanner-network with the target-network. The router can be artificially slowed down using different delaying and bandwidth parameters, like shown below. 
 
 ## Generate a custom lab
 
 Use ```python generate-lab.py``` to generate a docker lab enviroment with custom parameters:
 ```
 usage: generate-lab.py [-h] [--delay DELAY] [--loss LOSS] [--bandwidth BANDWIDTH] [--num-subnets NUM_SUBNETS]
-                       [--num-subnet-hosts NUM_SUBNET_HOSTS]
+                       [--num-subnet-hosts NUM_SUBNET_HOSTS] [--out-dir OUT_DIR]
 
 options:
   -h, --help            show this help message and exit
@@ -19,15 +18,15 @@ options:
   --bandwidth BANDWIDTH
                         Network bandwith of each hosts in kbit, mbit or gbit
   --num-subnets NUM_SUBNETS
-                        Number of subnets. Default is 1, starting from 172.0.1.0/24
+                        Number of subnets. Default is 2, starting from 10.0.1.0/24
   --num-subnet-hosts NUM_SUBNET_HOSTS
-                        Number of hosts per subnet. Default is 10, starting from 172.0.1.1-10
-
+                        Number of hosts per subnet. Default is 2, starting from 10.0.1.101
+  --out-dir OUT_DIR     Output directory of generated files
 ```
 The following tools are required:
 ```bash
-# Ubuntu
-apt install docker.io docker-compose -y
+# on Debian
+apt update && apt install docker.io docker-compose
 ```
 
 ## Example
@@ -35,43 +34,50 @@ apt install docker.io docker-compose -y
 
 The following command generates a lab environment with **3 subnets** and **10 hosts in each subnet**. Each host has a limited **bandwidth of 100kbit**, a response **delay of 200ms** and a **20% chance of package-loss**:
 ```
-python ./generate-lab.py --num-subnets 3 --num-subnet-hosts 10 --bandwidth 100kbit --delay 200ms --loss 20
+python ./generate-lab.py --num-subnets 3 --num-subnet-hosts 10 --bandwidth 100kbit --delay 200 --loss 20
 ```
 ### Spin up infrastructure
 
 ```bash
+# switch to lab directory
+cd ./generic-lab
+# clean up old containers and networks
+sudo docker-compose down --remove-orphans
+sudo docker network prune --force
 # build images
 sudo docker-compose build
 # spin up environment
 sudo docker-compose up -d 
 # apply network traffic limitations to containers 
 sudo sh ./traffic-control.sh add
-
-# clean-up environment after usage
-# sudo docker-compose down --remove-orphans
 ```
 
 ### Test setup (optional)
-To check if it worked, check if you can open the URL http://172.0.1.1 in your web browser.
+To check if it worked, log in to the ```scanner```-container and use curl, to request the webpage on http://10.0.1.101:
+```bash
+sudo docker exec -it scanner /bin/bash
+curl http://10.0.1.101
+```
 
-The check if the bandwidth limitation is active, try to download the file http://172.0.1.1/bigfile.zip with your web browser or using the terminal:
+To check if the bandwidth limitation is active, try to download the file http://10.0.1.101/bigfile.zip.
 
 ```bash
-curl -L http://172.0.1.1/bigfile.zip --output /tmp/
+curl -L http://10.0.1.101/bigfile.zip --output /tmp/bigfile.zip
 ```
+The download speed should be very limited.
 
 To see if the response delay and package loss is active, you can use a ping command:
 
 ```bash
-ping 172.0.1.1
+ping 10.0.1.101
 ```
 The response time is exactly 200ms and some packages get lost, like package #3 in the following example:
 ```bash
-PING 172.0.1.1 (172.0.1.1) 56(84) bytes of data.
-64 bytes from 172.0.1.1: icmp_seq=1 ttl=64 time=200 ms
-64 bytes from 172.0.1.1: icmp_seq=2 ttl=64 time=200 ms
-64 bytes from 172.0.1.1: icmp_seq=4 ttl=64 time=200 ms
-64 bytes from 172.0.1.1: icmp_seq=5 ttl=64 time=200 ms
+PING 10.0.1.101 (10.0.1.101) 56(84) bytes of data.
+64 bytes from 10.0.1.101: icmp_seq=1 ttl=64 time=200 ms
+64 bytes from 10.0.1.101: icmp_seq=2 ttl=64 time=200 ms
+64 bytes from 10.0.1.101: icmp_seq=4 ttl=64 time=200 ms
+64 bytes from 10.0.1.101: icmp_seq=5 ttl=64 time=200 ms
 ```
 
 ### Run tests
--- data/lab/generic-lab/generate-lab.py
+++ data/lab/generic-lab/generate-lab.py
@@ -5,12 +5,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--delay", type=int, default=1, help="Network delay in milliseconds")
 parser.add_argument("--loss", type=int, default=1, help="Network package loss rate in percent")
 parser.add_argument("--bandwidth", type=str, default="100kbit", help="Network bandwith of each hosts in kbit, mbit or gbit")
-parser.add_argument("--num-subnets", type=int, default=1, help="Number of subnets. Default is 1, starting from 172.0.1.0/24")
-parser.add_argument("--num-subnet-hosts", type=int, default=10, help="Number of hosts per subnet. Default is 10, starting from 172.0.1.1-10")
-parser.add_argument("--dir", type=str, default="generic-lab", help="Output directory")
+parser.add_argument("--num-subnets", type=int, default=2, help="Number of subnets. Default is 2, starting from 10.0.1.0/24")
+parser.add_argument("--num-subnet-hosts", type=int, default=2, help="Number of hosts per subnet. Default is 2, starting from 10.0.1.101-102")
+parser.add_argument("--out-dir", type=str, default="generic-lab", help="Output directory")
 args = parser.parse_args()
 
-OUT_PATH=os.path.join(os.getcwd(), args.dir)
+OUT_PATH=os.path.join(os.getcwd(), args.out_dir)
 DOCKERFILE="dockerfile"
 HTDOCS_DIR="htdocs"
 COMPOSEFILE="compose.yml"
@@ -21,7 +21,8 @@ TC_BANDWIDTH=args.bandwidth
 TC_LATENCY="500ms" # packets with higher latency get dropped
 TC_LOSS=str(args.loss)+"%"
 TC_DELAY=str(args.delay)+"ms"
-IP_PREFIX="172.0."
+PREFIX_TARGET_NET="10.0."
+PREFIX_SCANNER_NET="172.18."
 NUM_SUBNET=args.num_subnets
 NUM_SERVER_IN_SUBNET=args.num_subnet_hosts
 
@@ -35,15 +36,15 @@ os.mkdir(OUT_PATH)
 print (f"Create {HOSTSFILE}")
 with open(os.path.join(OUT_PATH, HOSTSFILE), "w") as f:
     for subnet_index in range(1, NUM_SUBNET + 1):     
-        for host_index in range(1,NUM_SERVER_IN_SUBNET + 1):            
-            f.write(f"{IP_PREFIX}{subnet_index}.{host_index}\n")
+        for host_index in range(101,NUM_SERVER_IN_SUBNET + 101):            
+            f.write(f"{PREFIX_TARGET_NET}{subnet_index}.{host_index}\n")
 
 
 # create file containing all class-C networks containing hosts
 print (f"Create {NETWORKSFILE}")
 with open(os.path.join(OUT_PATH, NETWORKSFILE), "w") as f:
     for subnet_index in range(1, NUM_SUBNET + 1):            
-        f.write(f"{IP_PREFIX}{subnet_index}.0/24\n")            
+        f.write(f"{PREFIX_TARGET_NET}{subnet_index}.0/24\n")            
 
 
 # create dockerfile if scannable host
@@ -75,24 +76,58 @@ with open(os.path.join(OUT_PATH, COMPOSEFILE), "w") as f:
     f.write(f"version: '3.8'\n\n")
 
     f.write(f"networks:\n")
-    f.write(f"  lab-network:\n")
+    f.write(f"  net_scanner:\n")
     f.write(f"    driver: bridge\n")
     f.write(f"    ipam:\n")
     f.write(f"      config:\n")
-    f.write(f"        - subnet: 172.0.0.0/16\n\n")
+    f.write(f"        - subnet: {PREFIX_SCANNER_NET}0.0/16\n")
+    f.write(f"    driver_opts:\n")
+    f.write(f"      com.docker.network.bridge.name: ethscanner\n")
+    f.write(f"  net_target:\n")
+    f.write(f"    driver: bridge\n")
+    f.write(f"    ipam:\n")
+    f.write(f"      config:\n")
+    f.write(f"        - subnet: 10.0.0.0/16\n\n")
 
     f.write(f"services:\n")
+    f.write(f"  router:\n")
+    f.write(f"    image: debian\n")
+    f.write(f"    hostname: router\n")
+    f.write(f"    container_name: router\n")
+    f.write(f"    command: sh -c 'apt-get update && apt-get install iputils-ping iproute2 iptables -y && sleep infinity'\n")
+    f.write(f"    cap_add:\n")
+    f.write(f"      - NET_ADMIN\n")
+    f.write(f"    sysctls:\n")
+    f.write(f"      - net.ipv4.ip_forward=1\n")
+    f.write(f"    networks:\n")
+    f.write(f"      net_scanner:\n")
+    f.write(f"        ipv4_address: {PREFIX_SCANNER_NET}0.2  # Gateway for scanner\n")
+    f.write(f"      net_target:\n")
+    f.write(f"        ipv4_address: {PREFIX_TARGET_NET}0.2   # gateway for target net\n")
+
+    f.write(f"  scanner:\n")
+    f.write(f"    image: debian\n")
+    f.write(f"    hostname: scanner\n")
+    f.write(f"    container_name: scanner\n")
+    f.write(f"    command: sh -c 'apt-get update && apt-get install iproute2 iputils-ping nmap wget -y && ip route add {PREFIX_TARGET_NET}0.0/16 via {PREFIX_SCANNER_NET}0.2 && sleep infinity'\n")
+    f.write(f"    networks:\n")
+    f.write(f"      net_scanner:\n")
+    f.write(f"        ipv4_address: {PREFIX_SCANNER_NET}0.66\n")
+    f.write(f"    cap_add:\n")
+    f.write(f"      - NET_ADMIN\n")
+
     for subnet_index in range(1, NUM_SUBNET + 1):     
-        for host_index in range(1,NUM_SERVER_IN_SUBNET + 1):
-            f.write(f"  lab-host-{subnet_index}-{host_index}:\n")
+        for host_index in range(101,NUM_SERVER_IN_SUBNET + 101):
+            f.write(f"  target-{PREFIX_TARGET_NET.replace('.','-')}{subnet_index}-{host_index}:\n")
             f.write(f"    build:\n")
             f.write(f"      context: .\n")
             f.write(f"      dockerfile: {DOCKERFILE}\n")
-            f.write(f"    container_name: lab-host-{subnet_index}-{host_index}\n")
-            f.write(f"    hostname: lab-host-{subnet_index}-{host_index}\n")
+            f.write(f"    container_name: target-{PREFIX_TARGET_NET.replace('.','-')}{subnet_index}-{host_index}\n")
+            f.write(f"    hostname: target-{PREFIX_TARGET_NET.replace('.','-')}{subnet_index}-{host_index}\n")
+            f.write(f"    command: sh -c 'ip route add {PREFIX_SCANNER_NET}0.0/24 via {PREFIX_TARGET_NET}0.2 && service apache2 start && sleep infinity'\n")
             f.write(f"    networks:\n")
-            f.write(f"      lab-network:\n")
-            f.write(f"        ipv4_address: 172.0.{subnet_index}.{host_index}\n")
+            f.write(f"      net_target:\n")
+            f.write(f"        ipv4_address: {PREFIX_TARGET_NET}{subnet_index}.{host_index}\n")
             f.write(f"    cap_add:\n")
             f.write(f"      - NET_ADMIN\n")
             f.write(f"    volumes:\n")
@@ -117,25 +152,21 @@ with open(os.path.join(OUT_PATH, TCFILE), "w") as f:
     f.write(f"TC_DELAY={TC_DELAY} # delay effects ping requests also\n\n")
 
     f.write(f'if [ "$1" = "add" ]; then\n')
-    f.write(f'  echo "adding traffic control to all containers..."\n\n')
-    for subnet_index in range(1, NUM_SUBNET + 1):
-        for host_index in range(1, NUM_SERVER_IN_SUBNET + 1):
-            #f.write(f"  sudo docker exec -it lab-host-{subnet_index}-{host_index} tc qdisc add dev eth0 root handle 1: netem loss $TC_LOSS delay $TC_DELAY\n")
-            #f.write(f"  sudo docker exec -it lab-host-{subnet_index}-{host_index} tc qdisc add dev eth0 parent 1: handle 2: tbf rate $TC_BANDWIDTH burst 16kbit latency $TC_LATENCY\n")   
-            f.write(f"  sudo docker exec lab-host-{subnet_index}-{host_index} tc qdisc add dev eth0 root handle 1: netem loss $TC_LOSS delay $TC_DELAY\n")
-            f.write(f"  sudo docker exec lab-host-{subnet_index}-{host_index} tc qdisc add dev eth0 parent 1: handle 2: tbf rate $TC_BANDWIDTH burst 16kbit latency $TC_LATENCY\n")   
+    f.write(f'  echo "adding traffic control to all containers..."\n')
+    f.write(f"  sudo docker exec router tc qdisc add dev eth0 root handle 1: netem loss $TC_LOSS delay $TC_DELAY\n")
+    f.write(f"  sudo docker exec router tc qdisc add dev eth0 parent 1: handle 2: tbf rate $TC_BANDWIDTH burst 16kbit latency $TC_LATENCY\n")  
     f.write(f'fi\n\n')
 
     f.write(f'if [ "$1" = "del" ]; then\n')
-    f.write(f'  echo "deleting traffic control from all containers..."\n\n')
-    for subnet_index in range(1, NUM_SUBNET + 1):
-        for host_index in range(1, NUM_SERVER_IN_SUBNET + 1):
-            f.write(f"  sudo docker exec -it lab-host-{subnet_index}-{host_index} tc qdisc del dev eth0 root 2> /dev/null\n")
+    f.write(f'  echo "deleting traffic control from all containers..."\n')
+    f.write(f"  sudo docker exec router tc qdisc del dev eth0 parent 1: handle 2: tbf rate $TC_BANDWIDTH burst 16kbit latency $TC_LATENCY\n")  
+    f.write(f"  sudo docker exec router tc qdisc del dev eth0 root handle 1: netem loss $TC_LOSS delay $TC_DELAY\n")
     f.write(f'fi\n\n')    
 
     f.write(f'echo "Done."')
 
 
-print ("\n")
-print ("# Start lab:\n")
-print ("sudo docker-compose down --remove-orphans && sudo docker-compose build && sudo docker-compose up -d && sudo sh ./traffic-control.sh add\n")
+print ("\n# Run the following commands to start the lab:\n")
+print (f"cd {OUT_PATH}")
+print ("sudo docker-compose down --remove-orphans && sudo docker network prune --force")
+print ("sudo docker-compose build && sudo docker-compose up -d && sudo sh ./traffic-control.sh add")
